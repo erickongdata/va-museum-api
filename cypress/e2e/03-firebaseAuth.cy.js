fe72ef1251234603162c2fd4eb30af56d7a412d7
Eric Kong <erickongdata@gmail.com>
2022-08-17 10:49:57 +0100

    Add User bookmarks Cypress tests

--- cypress/e2e/03-firebaseAuth.cy.js
+++ cypress/e2e/03-firebaseAuth.cy.js
@@ -5,6 +5,21 @@ const firstName = 'Frank';
 const email = 'frank@test-email.com';
 const password = 'Pass123';
 
+const loginUser = () => {
+  cy.visit(`${urlBase}/login`);
+  cy.get('#email').type(email);
+  cy.get('#user-password').type(password);
+  cy.get('button[type="submit"]').click();
+  cy.contains(firstName, { timeout: 20000 }).should('exist');
+};
+
+const logoutUser = () => {
+  cy.get('[data-cy="nav-user"]').click();
+  cy.get('[data-cy="drop-sign-out"]').click();
+  cy.url().should('eq', `${urlBase}/logout`);
+  cy.get('button[type="submit"]').click();
+};
+
 describe('Registered account works', () => {
   it('Sign up account test', () => {
     cy.visit(`${urlBase}/register`);
@@ -16,13 +31,30 @@ describe('Registered account works', () => {
     cy.contains('Passwords do not match').should('exist');
     cy.get('#user-password-confirmation').clear().type(password);
     cy.get('button[type="submit"]').click();
-    cy.get('[data-cy="nav-user"]').click();
-    cy.get('[data-cy="drop-sign-out"]').click();
-    cy.url().should('eq', `${urlBase}/logout`);
-    cy.get('button[type="submit"]').click();
+    cy.contains(firstName, { timeout: 20000 }).should('exist');
+    logoutUser();
   });
 
-  // it('Bookmarks are saved', () => {
-  //   cy.get('[data-cy="nav-mygallery"]').click();
-  // })
+  it.only('Bookmarks are saved', () => {
+    loginUser();
+    cy.get('[data-cy="search-input"]').type('Potter Beatrix peter rabbit');
+    cy.get('[data-cy="search-submit-btn"]').click();
+    cy.get('[data-cy="card-book"]')
+      .should('have.length', 15)
+      .each(($el, index) => {
+        if (index < 5) {
+          cy.wrap($el).click();
+        }
+      });
+    cy.visit(`${urlBase}/mygallery`);
+    cy.get('[data-cy="gallery-card"]').should('exist').should('have.length', 5);
+    logoutUser();
+    cy.visit(`${urlBase}/mygallery`);
+    cy.get('[data-cy="gallery-card"]').should('not.exist');
+
+    loginUser();
+    cy.visit(`${urlBase}/mygallery`);
+    cy.get('[data-cy="gallery-card"]').should('exist').should('have.length', 5);
+    logoutUser();
+  });
 });
