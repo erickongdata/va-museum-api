facc2283cd3f505a02f4f0f875bf37d591837237
Eric Kong <erickongdata@gmail.com>
2022-12-03 16:51:42 +0000

    Fix MyGallery Date sorting - more specific

--- src/utilities/sortFilterBookmarks.js
+++ src/utilities/sortFilterBookmarks.js
@@ -25,23 +25,23 @@ function sortFilterBookmarks(bookmarks, sorting, filtering) {
     );
   }
   if (sorting === 'date' || sorting === 'date-rev') {
-    const yearBc = /\d+\s?bc/i; // format: 134 BC or 134BC
-    const centuryBc = /\d+(st|nd|rd|th).*bc/i; // format: 1st or 14th century BC
-    const century = /\d+(st|nd|rd|th)/i; // format: 1st or 14th
+    const centuryBc = /\d+(st|nd|rd|th).*?century.*?bc/i; // format: 1st century BC or 14th century BC
+    const century = /\d+(st|nd|rd|th).*?century/i; // format: 1st or 14th century
+    const yearBc = /\d+.*?bc/i; // format: 134 BC or 134BC
     const year = /\d{4}/; // format: 1988
     const yearShort = /\d+/; // format: 123 or 289 AD
     return bookmarksFiltered.sort((a, b) => {
       const aDate =
-        parseInt(a.date.match(yearBc), 10) * -1 ||
         parseInt(a.date.match(centuryBc), 10) * -100 ||
         parseInt(a.date.match(century), 10) * 100 ||
+        parseInt(a.date.match(yearBc), 10) * -1 ||
         parseInt(a.date.match(year), 10) ||
         parseInt(a.date.match(yearShort), 10) ||
         9999;
       const bDate =
-        parseInt(b.date.match(yearBc), 10) * -1 ||
         parseInt(b.date.match(centuryBc), 10) * -100 ||
         parseInt(b.date.match(century), 10) * 100 ||
+        parseInt(b.date.match(yearBc), 10) * -1 ||
         parseInt(b.date.match(year), 10) ||
         parseInt(b.date.match(yearShort), 10) ||
         9999;
