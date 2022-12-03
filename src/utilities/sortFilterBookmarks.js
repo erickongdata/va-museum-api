2641d9299dbff2070106e1d63a017ae139afe73c
Eric Kong <erickongdata@gmail.com>
2022-12-03 17:25:33 +0000

    Simplify sortFilterBookmarks code

--- src/utilities/sortFilterBookmarks.js
+++ src/utilities/sortFilterBookmarks.js
@@ -1,15 +1,31 @@
 /* eslint no-nested-ternary: 0 */
 
+function dateConvertToNumber(dateString) {
+  const centuryBc = /\d+(st|nd|rd|th).*?century.*?bc/i; // format: 1st century BC or 14th century BC
+  const century = /\d+(st|nd|rd|th).*?century/i; // format: 1st or 14th century
+  const yearBc = /\d+.*?bc/i; // format: 134 BC or 134BC
+  const year = /\d{4}/; // format: 1988
+  const yearShort = /\d+/; // format: 123 or 289 AD
+
+  return (
+    parseInt(dateString.match(centuryBc), 10) * -100 ||
+    parseInt(dateString.match(century), 10) * 100 ||
+    parseInt(dateString.match(yearBc), 10) * -1 ||
+    parseInt(dateString.match(year), 10) ||
+    parseInt(dateString.match(yearShort), 10) ||
+    9999
+  );
+}
+
 function sortFilterBookmarks(bookmarks, sorting, filtering) {
   // filter bookmarks
-  let bookmarksFiltered;
-  if (filtering === '') {
-    bookmarksFiltered = [...bookmarks];
-  } else {
-    bookmarksFiltered = bookmarks.filter((book) =>
-      book.artist.toLowerCase().includes(filtering.toLowerCase())
-    );
-  }
+  const bookmarksFiltered =
+    filtering === ''
+      ? [...bookmarks]
+      : bookmarks.filter((book) =>
+          book.artist.toLowerCase().includes(filtering.toLowerCase())
+        );
+
   // Sort bookmarks
   if (sorting === 'added-rev') {
     return bookmarksFiltered.reverse();
@@ -25,26 +41,9 @@ function sortFilterBookmarks(bookmarks, sorting, filtering) {
     );
   }
   if (sorting === 'date' || sorting === 'date-rev') {
-    const centuryBc = /\d+(st|nd|rd|th).*?century.*?bc/i; // format: 1st century BC or 14th century BC
-    const century = /\d+(st|nd|rd|th).*?century/i; // format: 1st or 14th century
-    const yearBc = /\d+.*?bc/i; // format: 134 BC or 134BC
-    const year = /\d{4}/; // format: 1988
-    const yearShort = /\d+/; // format: 123 or 289 AD
     return bookmarksFiltered.sort((a, b) => {
-      const aDate =
-        parseInt(a.date.match(centuryBc), 10) * -100 ||
-        parseInt(a.date.match(century), 10) * 100 ||
-        parseInt(a.date.match(yearBc), 10) * -1 ||
-        parseInt(a.date.match(year), 10) ||
-        parseInt(a.date.match(yearShort), 10) ||
-        9999;
-      const bDate =
-        parseInt(b.date.match(centuryBc), 10) * -100 ||
-        parseInt(b.date.match(century), 10) * 100 ||
-        parseInt(b.date.match(yearBc), 10) * -1 ||
-        parseInt(b.date.match(year), 10) ||
-        parseInt(b.date.match(yearShort), 10) ||
-        9999;
+      const aDate = dateConvertToNumber(a.date);
+      const bDate = dateConvertToNumber(b.date);
       return sorting === 'date' ? aDate - bDate : bDate - aDate;
     });
   }
